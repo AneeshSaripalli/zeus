df785eae62b0a601f112c10bd4c74a98e487610d
Aneesh Saripalli <aneeshsaripalli@gmail.com>
2020-04-25 00:16:38 -0500

    Css changes:

--- client/src/pages/landing/Landing.tsx
+++ client/src/pages/landing/Landing.tsx
@@ -20,16 +20,24 @@ const Landing: React.FC<IProps> = (props: IProps): JSX.Element => {
                 Making resource conservation fun
                 </div>
             <div className="flex-grow" />
-            <ArrowDown color="#000000" className="bkg-white clr-accent clickable" id="click-arrow" />
+            <a href='#info'>
+                <ArrowDown color="#000000" className="bkg-white clr-accent clickable" id="click-arrow" />
+            </a>
         </div>
 
 
-        <div className="flexbox flex-column flex-center" style={{ backgroundColor: 'rgba(240,240,240)', height: '50%', textAlign: 'center', paddingTop: '10px' }}>
+        <div className="flexbox flex-column flex-center" id="info" style={{ height: '100vh', backgroundColor: 'rgba(240,240,240)', textAlign: 'center', paddingTop: '50px' }}>
+            <a href='info' />
             <div className="clr-accent title anim-delay-1s" id="whatwedo" style={{ fontSize: '40px' }}>
-                <span className="hover-grow bkg-accent clr-white rounded pd-sm">
+                <span className="hover-grow clr-accent rounded pd-sm fw-600">
                     What We Do
-                     </span>
+                </span>
             </div>
+            <div className="clr-black body fw-600 font-xlg" style={{ marginTop: '60px', marginLeft: "10%", marginRight: '10%' }}>
+                <div>We've solved the incentives problem for reducing power consumption.</div>
+
+            </div>
+
         </div>
 
     </div >
