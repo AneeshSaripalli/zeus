67f4ddf925410f8279dcb89268fd7c2102cd057d
Aneesh Saripalli <aneeshsaripalli@gmail.com>
2020-04-24 20:03:35 -0500

    Css is cool

--- client/src/pages/landing/Landing.tsx
+++ client/src/pages/landing/Landing.tsx
@@ -1,24 +1,39 @@
 import React from 'react';
 import './Landing.css'
+import ArrowDown from '@ant-design/icons/ArrowDownOutlined'
+import Icon from '@ant-design/icons/lib/components/Icon';
 
 type IProps = {
 
 }
 
 const Landing: React.FC<IProps> = (props: IProps): JSX.Element => {
-    return (
-        <div className="title bkg-accent flexbox flex-column flex-center fadein" id="landing" style={{ height: 'calc(50vh)', textAlign: 'center' }}>
+    return (<div>
+        <div className="title bkg-accent flexbox flex-column flex-center fadein" id="landing" style={{ height: 'calc(100vh - var(--header-px))', textAlign: 'center', paddingBottom: 30 }}>
             <div className="flex-grow" />
             <div className="clr-white title" style={{ fontSize: '60px' }}>
                 <span className="hover-grow">
                     Zeus
-                </span>
+                    </span>
             </div>
-            <div className="clr-white subtitle font-lg">
-                Making resource conservation fun.
-            </div>
+            <div className="bkg-white rounded clr-accent pd-sm subtitle fadein font-lg" id="subtitle">
+                Making resource conservation fun..
+                </div>
             <div className="flex-grow" />
-        </div>)
+            <ArrowDown color="#000000" className="bkg-white clr-accent clickable" id="click-arrow" />
+        </div>
+
+
+        <div className="flexbox flex-column flex-center" style={{ backgroundColor: 'rgba(240,240,240)', height: '50%', textAlign: 'center', paddingTop: '10px' }}>
+            <div className="clr-accent title anim-delay-1s" id="whatwedo" style={{ fontSize: '40px' }}>
+                <span className="hover-grow bkg-accent clr-white rounded pd-sm">
+                    What We Do
+                     </span>
+            </div>
+        </div>
+
+    </div>
+    )
 };
 
 export default Landing;
